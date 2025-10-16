7b93828d42137f22fa098608f4315e57308e6768
Karan Singh <kvnisad18114982@gmail.com>
2025-10-16 14:48:16 +0530

    fix error

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -1,22 +1,23 @@
-class ApiError extends Error {
-    constructor(
-        statusCode,
-        message = 'Something went wrong',
-        error = [],
-        stack = ""
-    ){
-        super(message);
-        this.statusCode = statusCode;
-        this.message = message;
-        this.errors = errors;
-        this.data = null;
-        this.success = false;
-        if(stack){
-            this.stack = stack;
-        } else {
-            Error.captureStackTrace(this, this.constructor);
-        }
+// ...existing code...
+export class ApiError extends Error {
+  constructor(
+    statusCode = 500,
+    message = "Something went wrong",
+    errors = null,
+    stack = ""
+  ) {
+    super(message);
+    this.name = "ApiError";
+    this.statusCode = statusCode;
+    this.message = message;
+    this.errors = errors;
+    this.data = null;
+    this.success = false;
+    if (stack) {
+      this.stack = stack;
+    } else if (Error.captureStackTrace) {
+      Error.captureStackTrace(this, this.constructor);
     }
+  }
 }
-
-export { ApiError };
+// ...existing code...
