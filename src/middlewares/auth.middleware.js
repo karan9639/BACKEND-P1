bf9b12a27f375cefb5c7bed6dba51598c7c8501e
Karan Singh <kvnisad18114982@gmail.com>
2025-11-01 09:34:18 +0530

    fix import issue

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,5 +1,5 @@
-import { ApiError } from "../utils/ApiError";
-import { User } from "../models/User.js";
+import { ApiError } from "../utils/ApiError.js";
+import { User } from "../models/user.model.js";
 import jwt from "jsonwebtoken";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
